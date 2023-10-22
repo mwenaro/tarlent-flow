aec38d5fe2e4833e0332b3292e3eba41dcea8658
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-10-22 12:23:24 +0300

    chore : add shadcn componets

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export * from "./organisms"
+// export {default as TailwindIndicator} from './tailwind-indicator'
+// export {default as ThemeToggle} from './theme-toggle'
+// export {default as ThemeProvider} from './theme-provider'
---
+++ components/organisms/index.ts
@@ -0,0 +1,2 @@
+export { MainNav as MainNav } from "./main-nav"
+export { SiteHeader as SiteHeader } from "./site-header"
